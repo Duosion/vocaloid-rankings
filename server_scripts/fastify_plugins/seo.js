f4f5ce5e97803b6301ae1d1d9d54c65e01277214
Duosion <duosiont@hotmail.com>
2023-01-10 02:27:30 -0600

    optimized SEO.

--- server_scripts/fastify_plugins/seo.js
+++ server_scripts/fastify_plugins/seo.js
@@ -4,10 +4,7 @@ const workingDirectory = process.cwd()
 const plugin = (fastify, options, done) => {
 
     const seo = require(workingDirectory + "/src/seo.json");
-    if (seo.url === "glitch-default") {
-        seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-    }
-
+    
     fastify.addHook("onRequest", (req, reply, done) => {
         req.seo = seo
         
