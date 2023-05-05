826427634a911e24090fc5ea8606c7b0386c0d92
Duosion <duosiont@gmail.com>
2023-05-05 17:58:35 -0500

    Implement system language option.

--- server_scripts/fastify_plugins/localization.js
+++ server_scripts/fastify_plugins/localization.js
@@ -1,21 +1,11 @@
 const fp = require("fastify-plugin")
 const LanguageSetting = require("../pages/settings/enums/LanguageSetting")
+const acceptLanguageParser = require('accept-language-parser');
 
-const localizations = [
-    {
-        isoCode: "",
-        localization: require("../../localization/template.json")
-    },
-    {
-        isoCode: "en",
-        localization: require("../../localization/en.json")
-    },
-    {
-        isoCode: "ja",
-        localization: require("../../localization/jp.json")
-    }
-    
-]
+const localeLookup = {
+    'en': require("../../localization/en.json"),
+    'ja': require("../../localization/jp.json")
+}
 
 const plugin = (fastify, options, done) => {
 
@@ -25,11 +15,30 @@ const plugin = (fastify, options, done) => {
     fastify.addHook('onRequest', (request, reply, done) => {
         const cookies = request.parsedCookies
 
-        const localization = localizations[cookies.language || LanguageSetting.Default.id]
-        const table = {...localizations[LanguageSetting.English.id].localization, ...localization.localization}
+        const settingCookie = cookies.language
+        const localizationSetting = (settingCookie ? Number.parseInt(settingCookie) : 0) || LanguageSetting.Default.id
+        let isoCode = 'en'
+        let localization
+        switch (localizationSetting) {
+            case 0:
+                const headers = request.headers || {}
+                const acceptLanguage = headers['accept-language'] || 'en;q=1'
+                const languages = acceptLanguageParser.parse(acceptLanguage)
+                isoCode = languages ? languages[0].code : isoCode
+                localization = localeLookup[isoCode]
+                break
+            case 2:
+                isoCode = 'ja'
+                localization = localeLookup['ja']
+                break
+            default:
+                localization = localeLookup[isoCode];
+                break
+        }
+        const table = {...localeLookup['en'], ...localization}
         request.localization = table
         request.addHbParam('localization', table)
-        request.addHbParam('localizationIsoCode', localization.isoCode)
+        request.addHbParam('localizationIsoCode', isoCode)
 
         done()
     })
