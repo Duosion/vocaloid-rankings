27d613d155557d236ae5d35d457a47caed0fada0
Duosion <duosiont@gmail.com>
2023-05-05 18:01:07 -0500

    Add home page title.

--- server_scripts/pages/home/index.js
+++ server_scripts/pages/home/index.js
@@ -5,7 +5,8 @@ const modulePath = workingDirectory + "/server_scripts"
 
 // fastify routes
 const getPage = (request, reply) => {
-    return reply.view('pages/home.hbs', request.hbParams)
+  request.addHbParam('pageTitle', (request.localization || {})['home_rankings'])
+  return reply.view('pages/home.hbs', request.hbParams)
 }
 
 exports.register = (fastify, options, done) => {
