52f96c4ce718673ecc5cd6c2e307933489bab3c1
Duosion <duosiont@gmail.com>
2023-09-25 22:57:05 -0500

    Add icon property to filled button.

--- src/components/material/filled-button.tsx
+++ src/components/material/filled-button.tsx
@@ -1,24 +1,39 @@
 import Link from "next/link"
 import { MouseEventHandler } from "react"
+import { Icon } from "./icon"
 
 export function FilledButton(
     {
         text,
+        icon,
         href,
         onClick,
-        className
+        className = ''
     }: {
         text: string
+        icon?: string
         href?: string
         onClick?: MouseEventHandler
         className?: string
     }
 ) {
+    const iconElement = icon ? <Icon icon={icon}/> : undefined
     return (
         href ? (
-            <Link className={`text-base bg-primary text-on-primary h-[40px] px-[24px] rounded-full flex items-center justify-center relative before:transition-opacity before:absolute before:w-full before:h-full before:left-0 before:top-0 before:rounded-full before:opacity-0 before:hover:bg-on-primary before:hover:opacity-[0.12] transition-opacity ${className}`} href={href} onClick={onClick}>{text}</Link>
+            <Link
+                className={`text-base bg-primary text-on-primary h-[40px] px-[24px] rounded-full flex items-center justify-center relative before:transition-opacity before:absolute before:w-full before:h-full before:left-0 before:top-0 before:rounded-full before:opacity-0 before:hover:bg-on-primary before:hover:opacity-[0.12] transition-opacity ${className}`}
+                href={href}
+                onClick={onClick}>
+                {iconElement}
+                {text}
+            </Link>
         ) : (
-            <button className={`text-base bg-primary text-on-primary h-[40px] px-[24px] rounded-full flex items-center justify-center relative before:transition-opacity before:absolute before:w-full before:h-full before:left-0 before:top-0 before:rounded-full before:opacity-0 before:hover:bg-on-primary before:hover:opacity-[0.12] transition-opacity ${className}`} onClick={onClick}>{text}</button>
+            <button
+                className={`text-base bg-primary text-on-primary h-[40px] px-[24px] rounded-full flex items-center justify-center relative before:transition-opacity before:absolute before:w-full before:h-full before:left-0 before:top-0 before:rounded-full before:opacity-0 before:hover:bg-on-primary before:hover:opacity-[0.12] transition-opacity ${className}`}
+                onClick={onClick}>
+                {iconElement}
+                {text}
+            </button>
         )
     )
 }
