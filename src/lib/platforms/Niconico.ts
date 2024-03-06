4978401133d34bf8a2c28a309a0576d5d070158f
Duosion <duosiont@gmail.com>
2024-03-06 00:59:44 -0600

    Fix niconico fallback functions.

--- src/lib/platforms/Niconico.ts
+++ src/lib/platforms/Niconico.ts
@@ -11,64 +11,65 @@ const headers = {
     'x-Frontend-version': '0'
 }
 
+async function getViewsFallback(
+    videoId: VideoId
+): Promise<number | null> {
+    console.log('niconico views fallback')
+    const result = await fetch(nicoNicoVideoDomain + videoId, {
+        method: 'GET',
+    })
+    if (!result) return null
+
+    const text = await result.text()
+
+    const parsedHTML = parseHTML(text)
+    // parse data-api-data
+    const dataElement = parsedHTML.document.getElementById("js-initial-watch-data")
+    if (!dataElement) { return null }
+
+    const videoData = JSON.parse(dataElement.getAttribute("data-api-data") || '[]')?.video
+
+    const rawViews = videoData?.count?.view
+
+    return rawViews == undefined ? null : Number.parseInt(rawViews)
+}
+
+function getThumbnailsFallback(
+    videoId: VideoId
+): Promise<VideoThumbnails | null> {
+    return fetch(nicoNicoVideoDomain + videoId)
+        .then(response => response.text())
+        .then(text => {
+            const parsedHTML = parseHTML(text)
+            // parse data-api-data
+            const dataElement = parsedHTML.document.getElementById("js-initial-watch-data")
+            if (!dataElement) { return null }
+
+            const videoData = JSON.parse(dataElement.getAttribute("data-api-data") || '[]')?.video
+
+            const thumbnail = videoData?.thumbnail?.url
+            return thumbnail == undefined ? null : {
+                default: thumbnail,
+                quality: thumbnail
+            }
+        })
+        .catch(_ => { return null })
+}
+
 class NiconicoPlatform implements Platform {
 
-    // fallback to 
-    async getViewsFallback(
-        videoId: VideoId
-    ): Promise<number | null> {
-        console.log('niconico views fallback')
-        const result = await fetch(nicoNicoVideoDomain + videoId, {
-            method: 'GET',
-        })
-        if (!result) return null
-
-        const text = await result.text()
-
-        const parsedHTML = parseHTML(text)
-        // parse data-api-data
-        const dataElement = parsedHTML.document.getElementById("js-initial-watch-data")
-        if (!dataElement) { return null }
-
-        const videoData = JSON.parse(dataElement.getAttribute("data-api-data") || '[]')?.video
-
-        const rawViews = videoData?.count?.view
-        return rawViews == undefined ? null : Number.parseInt(rawViews)
-    }
-
-    getThumbnailsFallback(
-        videoId: VideoId
-    ): Promise<VideoThumbnails | null> {
-        return fetch(nicoNicoVideoDomain + videoId)
-            .then(response => response.text())
-            .then(text => {
-                const parsedHTML = parseHTML(text)
-                // parse data-api-data
-                const dataElement = parsedHTML.document.getElementById("js-initial-watch-data")
-                if (!dataElement) { return null }
-
-                const videoData = JSON.parse(dataElement.getAttribute("data-api-data") || '[]')?.video
-
-                const thumbnail = videoData?.thumbnail?.url
-                return thumbnail == undefined ? null : {
-                    default: thumbnail,
-                    quality: thumbnail
-                }
-            })
-            .catch(_ => { return null })
-    }
-
     // https://niconicolibs.github.io/api/nvapi/#tag/Video
-    async getViews(
+    getViews(
         videoId: VideoId
     ): Promise<number | null> {
         return fetch(`${nicoNicoAPIDomain}videos?watchIds=${videoId}`, {
             headers: headers
         }).then(res => res.json())
         .then(videoData => {
+            throw new Error()
             return videoData['data']['items'][0]['video']['count']['view']
         })
-        .catch(_ => { return this.getViewsFallback(videoId) })
+        .catch(_ => { return getViewsFallback(videoId) })
     }
 
     getThumbnails(
@@ -85,7 +86,7 @@ class NiconicoPlatform implements Platform {
                 quality: thumbnails['largeUrl'] || defaultThumbnail
             }
         })
-        .catch(_ => { return this.getThumbnailsFallback(videoId) })
+        .catch(_ => { return getThumbnailsFallback(videoId) })
     }
 
 }
