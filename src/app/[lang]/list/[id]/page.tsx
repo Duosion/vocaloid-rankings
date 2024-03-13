8e5f223fe6d01a801566eab8e965b7236a902d9a
Duosion <duosiont@gmail.com>
2024-03-13 01:53:19 -0500

    Adjust design of the list page.

--- src/app/[lang]/list/[id]/page.tsx
+++ src/app/[lang]/list/[id]/page.tsx
@@ -9,6 +9,11 @@ import { songRankingsFilters } from "../../rankings/page"
 import { SongRankingsFiltersValues } from "../../rankings/types"
 import { Divider } from "@/components/material/divider"
 import Image from "@/components/image"
+import { FilledButton } from "@/components/material/filled-button"
+import { FilledIconButton } from "@/components/material/filled-icon-button"
+import { IconButton } from "@/components/material/icon-button"
+import { getCustomThemeStylesheet, getImageMostVibrantColor } from "@/lib/material/material"
+import { Hct, SchemeVibrant, argbFromHex, argbFromRgb } from "@material/material-color-utilities"
 
 export default async function ListPage(
     {
@@ -23,12 +28,24 @@ export default async function ListPage(
     }
 ) {
 
-    // CHANGE COLOR BASED ON IMAGE AVERAGE COLOR?
-
     // convert the id parameter into a number; get song data
     const listId = Number(params.id)
     const list = !isNaN(listId) ? await getList(listId) : null
     if (!list) return notFound()
+
+    // CHANGE COLOR BASED ON IMAGE AVERAGE COLOR?
+    const vibrantColor = list.image ? await getImageMostVibrantColor(list.image) : null
+
+    // generate custom theme
+    let customThemeLightCss: string = ''
+    let customThemeDarkCss: string = ''
+    if (vibrantColor) {
+        const argbAverageColor = argbFromRgb(vibrantColor[0], vibrantColor[1], vibrantColor[2])
+        // dynamic theme config
+        const contrast = 0.3
+        customThemeLightCss = getCustomThemeStylesheet(new SchemeVibrant(Hct.fromInt(argbAverageColor), false, contrast)).join('')
+        customThemeDarkCss = getCustomThemeStylesheet(new SchemeVibrant(Hct.fromInt(argbAverageColor), true, contrast)).join('')
+    }
 
     // import language dictionary
     const lang = params.lang
@@ -47,15 +64,33 @@ export default async function ListPage(
 
     return (
         <section className="flex flex-col gap-5 w-full min-h-screen">
-            <section className="flex gap-5">
+            <style>{`
+                :root {
+                    ${customThemeLightCss}
+                }
+                [data-theme=dark] {
+                    ${customThemeDarkCss}
+                }
+            `}</style>
+            
+            <section className="flex gap-5 flex-col md:flex-row">
                 <figure>
-                    <Image src={list.image || ''} className='aspect-square h-full w-48 rounded-3xl object-cover' alt={list.names[lang] || ''} />
+                    <Image src={list.image || ''} className='h-48 w-48 mx-auto rounded-3xl object-cover' alt={list.names[lang] || ''} />
                 </figure>
-                <div className="flex flex-col gap-3 justify-end mb-3">
+                <section className="flex flex-1 flex-col gap-3 justify-end mb-3">
                     <h1 className="font-bold md:text-5xl md:text-left text-4xl text-center w-full">{list.names[lang]}</h1>
                     <h3 className="font-semibold text-primary md:text-left text-xl text-center w-full">{langDict.list_song_count.replace(':count', list.songIds.length.toString())}</h3>
-                    <p className="text-on-surface-variant text-xl text-left w-full whitespace-break-spaces">{list.descriptions[lang]}</p>
-                </div>
+                    <p className="text-on-surface-variant md:text-left text-center text-xl w-full whitespace-break-spaces">{list.descriptions[lang]}</p>
+                </section>
+                <section className="flex gap-3 items-end justify-center md:justify-end">
+                    <IconButton
+                        icon="delete"
+                    />
+                    <FilledButton
+                        text={langDict.list_edit}
+                        icon="edit"
+                    />
+                </section>
             </section>
             <Divider />
             <RankingsList
